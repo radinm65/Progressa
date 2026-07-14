67ebb1225d71548f9074292ee29e33ecdd5eb23b
Radin Motahhary <radinm654@gmail.com>
2026-07-14 22:21:58 +0330

    Profile page implemented

--- Frontend/src/assets/pages/Profile/Profile.jsx
+++ Frontend/src/assets/pages/Profile/Profile.jsx
@@ -1,8 +1,44 @@
-import './Profile.css'
+import { useOutletContext } from "react-router-dom";
+import "./Profile.css";
 
 export function Profile() {
-    return (
-        <>
-        </>
-    )
+  const { user } = useOutletContext();
+  return (
+    <>
+      <div className="profile-wrapper">
+        <div className="profile-header-div">
+          <img src={`/${user.profile_img}`} alt="" />
+          <div className="profile-info">
+            <p>{user.name}</p>
+            <p>{`@${user.username}`}</p>
+          </div>
+        </div>
+        <div className="infos-wrapper">
+          <h2>Personal Info</h2>
+          <div className="infos">
+            <div>
+              <h3>age</h3>
+              <p>{user.age}</p>
+            </div>
+            <div>
+              <h3>Height</h3>
+              <p>{user.height}</p>
+            </div>
+            <div>
+              <h3>Weight</h3>
+              <p>{user.weight}</p>
+            </div>
+            <div>
+              <h3>Gender</h3>
+              <p>{user.gender}</p>
+            </div>
+            <div>
+              <h3>BMI</h3>
+              <p>{+(user.weight / ((user.height / 100) * (user.height / 100))).toFixed(2)}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
